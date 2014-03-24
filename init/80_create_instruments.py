96785d22166552247c245b95e4442a6398affde2
Jens <danneaugroup@gmail.com>
2014-03-24 14:36:23 +0100

    remove creation of example instruments

--- init/80_create_instruments.py
+++ init/80_create_instruments.py
@@ -1,9 +1,9 @@
-example1 = qt.instruments.create('example1', 'example', address='GPIB::1', reset=True)
-dsgen = qt.instruments.create('dsgen', 'dummy_signal_generator')
-pos = qt.instruments.create('pos', 'dummy_positioner')
-combined = qt.instruments.create('combined', 'virtual_composite')
-combined.add_variable_scaled('magnet', example1, 'chA_output', 0.02, -0.13, units='mT')
-#combined.add_variable_combined('waveoffset', [{
+#    example1 = qt.instruments.create('example1', 'example', address='GPIB::1', reset=True)
+#    dsgen = qt.instruments.create('dsgen', 'dummy_signal_generator')
+#    pos = qt.instruments.create('pos', 'dummy_positioner')
+#    combined = qt.instruments.create('combined', 'virtual_composite')
+#    combined.add_variable_scaled('magnet', example1, 'chA_output', 0.02, -0.13, units='mT')
+#    combined.add_variable_combined('waveoffset', [{
 #    'instrument': dmm1,
 #    'parameter': 'ch2_output',
 #    'scale': 1,
